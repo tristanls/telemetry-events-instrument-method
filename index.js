d22405f914911dee8bd74c2cbf51d383c8ff7006
Tristan Slominski <tristan.slominski@gmail.com>
2020-01-11 08:49:41 -0600

    Prob: Cannot instrument async methods.
    Solv: Expand API to allow for instrumentation of async and sync methods.

--- index.js
+++ index.js
@@ -12,9 +12,140 @@ const markTime = require("mark-time");
     * `telemetry`: _Object_ Telemetry helpers.
       * `logs`: _Object_ `telemetry-events-log` instance.
       * `metrics`: _Object_ `telemetry-events-quantify` instance.
+  * Return: _Function_ `async function(dynamic){}` Instrumented method to call.
+*/
+exports.async = config =>
+{
+    if (!config.target[config.method]
+        || typeof config.target[config.method] != "function")
+    {
+        throw new Error(`config.target["${config.method}"] is not a function and cannot be instrumented`);
+    }
+    /*
+      * `dynamic`: _Object_
+        * `args`: _Array_ Arguments to call the instrumented method with.
+        * `argsToLog`: _Array_ Redacted arguments to log.
+        * `errorLevel`: _String_ _(Default: `error`)_ Level at which to emit log
+            when error occurs.
+        * `errorTraceTag`: _Boolean_ _(Default: `true`)_ If `true` and an error
+            occurs, trace span will be tagged with `error = true`. No tag if set
+            to `false`.
+        * `metadata`: _Object_ Parent context.
+        * `parentSpan`: _TraceTelemetryEvents.Span_ _(Default: \`undefined\`)_
+            Parent span to inherit from if this call should be traced.
+        * `targetMetadata`: _Object_ Optional target specific metadata,
+            will be constructed if not provided.
+        * `tenantId`: _String_ Base64url encoded tenant id.
+    */
+    return async dynamic =>
+    {
+        dynamic.targetMetadata = dynamic.targetMetadata ||
+        {
+            module: config.target.name,
+            version: config.target.version
+        };
+        const targetMetadata = extend(true, clone(dynamic.metadata),
+            {
+                target:
+                {
+                    method: config.method
+                }
+            },
+            {
+                target: dynamic.targetMetadata
+            }
+        );
+        if (config.telemetry && config.telemetry.logs)
+        {
+            config.telemetry.logs.log("info", `attempting ${config.method}`, targetMetadata,
+                {
+                    target:
+                    {
+                        args: dynamic.argsToLog
+                    }
+                }
+            );
+        }
+        let traceSpan;
+        if (dynamic.parentSpan)
+        {
+            traceSpan = dynamic.parentSpan.childSpan(config.method, dynamic.targetMetadata);
+        }
+        const context = config.noContext ? undefined : (
+                {
+                    parentSpan: traceSpan,
+                    provenance: dynamic.metadata.provenance,
+                    tenantId: dynamic.tenantId
+                }
+            );
+        const epilog = (error, data, startTime) =>
+        {
+            const elapsedTime = markTime() - startTime;
+            if (config.telemetry && config.telemetry.metrics)
+            {
+                config.telemetry.metrics.gauge("latency",
+                    {
+                        unit: "ms",
+                        value: elapsedTime,
+                        metadata: clone(targetMetadata)
+                    }
+                );
+            }
+            if (error)
+            {
+                if (config.telemetry && config.telemetry.logs)
+                {
+                    config.telemetry.logs.log(dynamic.errorLevel ? dynamic.errorLevel : "error", `${config.method} failed`, targetMetadata,
+                        {
+                            target:
+                            {
+                                args: dynamic.argsToLog
+                            },
+                            error,
+                            stack: error.stack
+                        }
+                    );
+                }
+                if (traceSpan)
+                {
+                    if (dynamic.errorTraceTag !== false)
+                    {
+                        traceSpan.tag("error", true);
+                    }
+                    traceSpan.finish();
+                }
+                throw error;
+            }
+            if (traceSpan)
+            {
+                traceSpan.finish();
+            }
+            return data;
+        };
+        const startTime = markTime();
+        try
+        {
+            const data = await config.target[config.method](...dynamic.args, context);
+            return epilog(undefined, data, startTime);
+        }
+        catch (error)
+        {
+            return epilog(error, undefined, startTime);
+        }
+    }
+};
+
+/*
+  * `config`: _Object_
+    * `method`: _String_ Name of method on `target` to instrument.
+    * `noContext`: _Boolean_ If `true`, do not include context in method call.
+    * `target`: _Object_ Target to instrument.
+    * `telemetry`: _Object_ Telemetry helpers.
+      * `logs`: _Object_ `telemetry-events-log` instance.
+      * `metrics`: _Object_ `telemetry-events-quantify` instance.
   * Return: _Function_ `function(dynamic, callback){}` Instrumented method to call.
 */
-module.exports = config =>
+exports.sync = config =>
 {
     if (!config.target[config.method]
         || typeof config.target[config.method] != "function")
